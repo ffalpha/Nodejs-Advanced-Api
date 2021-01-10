64b7164e1d2781e3cea889d45fa8781aa5224215
Kalana Mihiranga <kalanam217@gmail.com>
2021-01-10 17:46:44 +0530

    Updated gitignore

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ const app =express();
 const PORT =process.env.PORT || 5000
 
 app.listen(PORT, () => {
-    console.log('App listening on port 3000!');
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
